233f9bf651ddab9e27795937173a9f90ccf222cd
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-05-16 14:08:06 +0530

    Added changelog basics. TODO, put an actual changelog instead of the help page

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
 # Updates local repo and then builds
 
+echo "Updating local repository..."
+
 git stash # Store user changes
 git pull
 git stash pop 
 
+echo "Building local repository..."
+
 yarn deploy
+
+read -p "App Updated. Do you wish to view changelog? [y/n]  " showupdate
+
+if [ $showupdate = "y" ]
+then
+  $BROWSER ./changelog.html
+fi
